8ef23d8c2c2ea0a10898b4500e0d44917fff5dd6
ShreejanRegmi <35588108+ShreejanRegmi@users.noreply.github.com>
2019-06-17 11:24:11 +0545

    Add files via upload

---
+++ CSY2038_Scripts/run25.sql
@@ -0,0 +1,41 @@
+/*
+Group Number: 25
+File Name: run25.sql
+Group Members:  Shreejan Regmi - 18417132
+				Neetu Kumari Das- 18406487
+				Saurav Khatiwada - 18413649
+				Hira Bahadur Rana Kauchha - 18413703
+Module Name: Database 2
+Module Code: CSY2038			
+*/
+
+--@E:\CSY2038_Scripts\run25.sql;
+
+--Run query to run whole script
+
+--Create script
+@E:\CSY2038_Scripts\create25.sql;
+
+--Constraint script
+@E:\CSY2038_Scripts\constraints25.sql;
+
+--Insert script
+@E:\CSY2038_Scripts\insert25.sql;
+
+--Procedure script
+@E:\CSY2038_Scripts\procedures25.sql;
+
+--Functions script
+@E:\CSY2038_Scripts\functions25.sql;
+
+--Triggers script
+@E:\CSY2038_Scripts\triggers25.sql;
+
+--Cursors script
+@E:\CSY2038_Scripts\cursors25.sql;
+
+--Views script
+@E:\CSY2038_Scripts\views25.sql;
+
+--Packages script
+@E:\CSY2038_Scripts\packages25.sql;
